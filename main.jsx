1545661fb1c6d308d9d1ba341489314dbb95aae3
danielreis <danielreis00@gmail.com>
2017-05-10 22:56:29 +0100

    Remove unecessary code

--- main.jsx
+++ main.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 
-import Root from './src/config/Root';
 import App from './src/components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
