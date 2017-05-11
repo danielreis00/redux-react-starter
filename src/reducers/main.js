d12bf72ba4b975c0ab3f56246fcbf175c1497fd8
danielreis <danielreis00@gmail.com>
2017-05-11 22:04:55 +0100

    Add dummy reducer

--- src/reducers/main.js
+++ src/reducers/main.js
@@ -1,9 +1,12 @@
 import { getInitialState } from '../utils/initialState';
+import * as types from '../constants/actionsTypes';
 
 const initialState = getInitialState();
 
 export const reducer = function (state = initialState, action = 'DEFAULT') {
   switch (action.type) {
+    case types.DO_DUMMY_SUCCESS:
+      return state.mergeDeep({ dummy: action.payload });
     default:
       return state;
   }
