21dd1b9b1ae30d740ae3329311f7c0e581e8bada
danielreis <danielreis00@gmail.com>
2017-05-11 22:04:46 +0100

    Transform App into a container and trigger a dummy action

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { doDummy } from '../actions/actions';
 
-class App extends Component {
+class AppContainer extends Component {
+  componentDidMount() {
+    this.props.onDummy(100);
+  }
+
   render() {
     return (
-      <h1 className="dr-header">Hello to my Redux-React boilerplate</h1>
+      <div>
+        <h1 className="dr-header">Hello to my Redux-React boilerplate</h1>
+        <div>Dummy: {this.props.dummy}</div>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+    dummy: state.get('dummy'),
+});
+
+const App = connect(
+  mapStateToProps,
+  {onDummy: doDummy}
+)(AppContainer);
 
 export default App;
